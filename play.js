f78f90cde28b5e57e0cb38e1968ad446c30954af
Mike Auty <m@auty.ca>
2021-11-03 20:16:52 +0000

    exported setupInput function

--- play.js
+++ play.js
@@ -1,26 +1,11 @@
 const log = console.log
 
 const { connect } = require('./client')
+const { setupInput } = require('./input')
 
-const setupInput = () => {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  const handleUserInput = () => {
-    stdin.on('data', (key) => {
-      // \u0003 maps to ctrl+c input
-      if (key === '\u0003') {
-        process.exit();
-      }
-    })
-    
-  }
-  stdin.on('data', handleUserInput)
-  return stdin;
-}
 
 log("Connecting ...");
 connect();
+
+
 setupInput();
